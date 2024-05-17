d7c47a6e71e4ce557758a53740de4e8c2893f3ee
canka sakti <cankasakti02@gmail.com>
2024-05-17 08:11:11 +0700

    Fix Transaction On Going

--- src/Application/Transactions/Queries/TransactionOnGoingDto.cs
+++ src/Application/Transactions/Queries/TransactionOnGoingDto.cs
@@ -6,6 +6,9 @@ public class TransactionOnGoingDTO
 {
     public string? Id { get; init; }
     public string? TransactionNum { get; init; }
+    public string? NameUnit { get; init; }
+    public string? TypeUnit { get; init; }
+    public string? ImgUnit { get; init; }
     public string? UserName { get; init; }
     public string? ReceiverName { get; init; }
     public string? ReceiverHp { get; init; }
@@ -17,6 +20,8 @@ public class TransactionOnGoingDTO
     public int StatusTransaction { get; init; }
     public bool IsBuy { get; init; }
     public DateTimeOffset Created { get; set; }
+    public DateOnly? DateRent { get; set; }
+    public DateOnly? DateReturn { get; set; }
 
     private class Mapping : Profile
     {
@@ -24,7 +29,13 @@ public class TransactionOnGoingDTO
         {
             CreateMap<Transaction, TransactionOnGoingDTO>()
             .ForMember(d => d.UserName, opt => opt.MapFrom(src => src.User.UserName))
-            .ForMember(d => d.IsBuy, opt => opt.MapFrom(src => src.DetailBuy != null && src.DetailRents == null ? true : src.DetailRents != null ? false : (bool?)null));
+            .ForMember(d => d.NameUnit, opt => opt.MapFrom(src => src.Unit.NameUnit))
+            .ForMember(d => d.TypeUnit, opt => opt.MapFrom(src => src.Unit.TypeUnit))
+            .ForMember(d => d.ImgUnit, opt => opt.MapFrom(src => src.Unit.ImgUnit))
+            .ForMember(d => d.IsBuy, opt => opt.MapFrom(src => src.DetailBuy != null && src.DetailRents == null ? true : src.DetailRents != null ? false : (bool?)null))
+            .ForMember(d => d.DateRent, opt => opt.MapFrom(src => src.DetailRents != null ? src.DetailRents.DateRent : (DateOnly?)null))
+            .ForMember(d => d.DateReturn, opt => opt.MapFrom(src => src.DetailRents != null ? src.DetailRents.DateReturn : (DateOnly?)null));
+            ;
         }
     }
 }
